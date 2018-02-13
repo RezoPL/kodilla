8588a57967dbe0fd7f785f1a40d9f315065d5ca0
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-13 17:31:37 +0100

    prettifying code

--- ajax_api/12.6/js/scripts.js
+++ ajax_api/12.6/js/scripts.js
@@ -4,19 +4,19 @@ var countriesList = $('#countries');
 $('#search').click(searchCountries);
 
 function searchCountries() {
- 	var countryName = $('#country-name').val();
-	
-	if(!countryName.length) countryName = 'Poland';
-		$.ajax({
-  			url: url + countryName,
-  			method: 'GET',
-  			success: showCountriesList
-  		});
+    var countryName = $('#country-name').val();
+
+    if (!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
 }
 
 function showCountriesList(resp) {
-  countriesList.empty();
-  resp.forEach(function(item){
-   	$('<li>').text(item.name + ', ' + item.capital).appendTo(countriesList);
-});
+    countriesList.empty();
+    resp.forEach(function(item) {
+        $('<li>').text(item.name + ', ' + item.capital).appendTo(countriesList);
+    });
 }
