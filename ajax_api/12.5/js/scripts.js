0d812cbf952d011ae48e040218037d0484229ae7
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-14 21:21:53 +0100

    repair error in line 34 of scripts.js

--- ajax_api/12.5/js/scripts.js
+++ ajax_api/12.5/js/scripts.js
@@ -3,34 +3,34 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-  $.getJSON(prefix + quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
 }
 
 $.ajaxSetup({ cache: false });
 
 function createTweet(input) {
-  var data = input[0];
+    var data = input[0];
 
-  var quoteText = $(data.content)
-    .text()
-    .trim();
-  var quoteAuthor = data.title;
+    var quoteText = $(data.content)
+        .text()
+        .trim();
+    var quoteAuthor = data.title;
 
-  if (!quoteAuthor.length) {
-    quoteAuthor = "Unknown author";
-  }
-  
-  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
 
-  if (tweetText.length > 140) {
-    getQuote();
-  } else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $(".quote").text(quoteText);
-    $(".author").text("Author: " + quoteAuthor);
-    $(".tweet").attr("href", tweet);
-  }
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $(".quote").text(quoteText);
+        $(".author").text("Author: " + quoteAuthor);
+        $(".tweet").attr("href", tweet);
+    }
 }
-$(document).ready(function){
- $(".trigger").click(getQuote);
-};
+$(document).ready(function() {
+    $(".trigger").click(getQuote);
+});
